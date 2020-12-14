23045a4b547383cc8a4cb4b23a47444827553ecf
spacehaz2 <spacehaz@gmail.com>
2020-12-14 15:20:48 +0300

    additional filter

--- app.js
+++ app.js
@@ -2,19 +2,12 @@ const express = require('express')
 const app = express()
 const PORT = 3030
 const bodyParser = require('body-parser')
-const bd = require('./bd/1-999.json')
 const cors = require('cors')
+const router = require('./routes')
 
 app.use(cors())
 app.use(bodyParser.json())
-app.get('/', (req, res) => {
-  const { q } = req.query
-  if (!q) {
-    return res.send(bd)
-  }
-  const foundPoems = bd.filter(item => item.fields.text.includes(q))
-  res.send(foundPoems)
-})
+app.use('/', router)
 
 
 app.listen(PORT, _ => {
