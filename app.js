408f7b9ecd9999c5276b32dac0a5646ad5ebc2a6
spacehaz2 <spacehaz@gmail.com>
2020-12-14 13:36:07 +0300

    edited initial logic

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const PORT = 3030
+const bodyParser = require('body-parser')
+const bd = require('./bd/1-999.json')
+
+
+app.use(bodyParser.json())
+app.get('/', (req, res) => {
+  const { q } = req.query
+  if (!q) {
+    return res.send(bd)
+  }
+  const foundPoems = bd.filter(item => item.fields.text.includes(q))
+  res.send(foundPoems)
+})
+
+
+app.listen(PORT, _ => {
+  console.log(`listening to ${PORT}`)
+})
