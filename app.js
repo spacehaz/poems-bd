88c0008554b826d99e6fc4c01e91c29d9996e992
spacehaz2 <spacehaz@gmail.com>
2020-12-16 12:46:35 +0300

    additional PORT

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 3030
+const PORT = process.env.PORT || 8080;
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const router = require('./routes')
