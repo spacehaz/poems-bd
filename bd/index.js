59090c65c8895438d3794b4009463e670bea3be0
spacehaz2 <spacehaz@gmail.com>
2021-04-04 19:02:30 +0300

    added

--- bd/index.js
+++ bd/index.js
@@ -1,6 +1,3 @@
 const json1 = require('./1-999.json')
-const json2 = require('./1000-1999.json')
-const json3 = require('./2000-2999.json')
-const json4 = require('./3000-3999.json')
-const json5 = require('./4000-4999.json')
-module.exports = [...json1, ...json2, ...json3, ...json4, ...json5]
+
+module.exports = json1
