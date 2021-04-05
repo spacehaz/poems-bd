54b0165b26d7f0ec161906364ff6e751580a5d82
spacehaz2 <spacehaz@gmail.com>
2021-04-05 18:03:26 +0300

    added fix for static

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const PORT = process.env.PORT || 8080;
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const router = require('./routes')
+const path = require('path')
 
 app.use(cors())
+app.use(express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.json())
 app.use('/', router)
 
